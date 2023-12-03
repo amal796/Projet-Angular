3a080eb9317ca6a10f1fedd40be7ecd4d2433a45
hazem gdara <hazem.gdara@laserinformatique.com>
2023-12-03 23:08:35 +0100

    crud and 360

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,37 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { TableComponent } from './table/table.component';
-import { AjoutComponent } from './ajout/ajout.component';
-import { ModifComponent } from './modif/modif.component';
-import { AddUniversiteComponent } from './Universite/Components/AddUniversite/add-universite/add-universite.component';
-import { ListUniversiteComponent } from './Universite/Components/ListUniversite/list-universite/list-universite.component';
-import { UpdateUniversiteComponent } from './Universite/Components/UpdateUniversite/update-universite/update-universite.component';
-import { E404Component } from './e404/e404.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {TableComponent} from './table/table.component';
+import {AjoutComponent} from './ajout/ajout.component';
+import {ModifComponent} from './modif/modif.component';
+import {AddUniversiteComponent} from './Universite/Components/AddUniversite/add-universite/add-universite.component';
+import {
+    ListUniversiteComponent
+} from './Universite/Components/ListUniversite/list-universite/list-universite.component';
+import {
+    UpdateUniversiteComponent
+} from './Universite/Components/UpdateUniversite/update-universite/update-universite.component';
+import {E404Component} from './e404/e404.component';
+import {ChambreComponent} from "./chambre/chambre.component";
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent },
-  { path: 'Login', component: LoginComponent },
-  { path: 'Table', component: TableComponent },
-  { path: 'Ajouter', component: AjoutComponent },
-  { path: 'Modifier', component: ModifComponent },
-  { path: 'AddUniversite', component: AddUniversiteComponent },
-  { path: 'ListUniversite', component: ListUniversiteComponent },
-  { path: 'UpdateUniversite', component: UpdateUniversiteComponent },
-  { path: '**', component: E404Component },
+    {path: '', component: DashboardComponent},
+    {path: 'Login', component: LoginComponent},
+    {path: 'Table', component: TableComponent},
+    {path: 'Ajouter', component: AjoutComponent},
+    {path: 'chambres', component: ChambreComponent},
+    {path: 'Modifier', component: ModifComponent},
+    {path: 'AddUniversite', component: AddUniversiteComponent},
+    {path: 'ListUniversite', component: ListUniversiteComponent},
+    {path: 'UpdateUniversite', component: UpdateUniversiteComponent},
+    {path: '**', component: E404Component},
 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,47 +1,58 @@
-
-
-
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TableComponent } from './table/table.component';
-import { AjoutComponent } from './ajout/ajout.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { MenuComponent } from './menu/menu.component';
-import { ModifComponent } from './modif/modif.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { LoginComponent } from './login/login.component';
-import { AddUniversiteComponent } from './Universite/Components/AddUniversite/add-universite/add-universite.component';
-import { ListUniversiteComponent } from './Universite/Components/ListUniversite/list-universite/list-universite.component';
-import { UpdateUniversiteComponent } from './Universite/Components/UpdateUniversite/update-universite/update-universite.component';
-import { E404Component } from './e404/e404.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {TableComponent} from './table/table.component';
+import {AjoutComponent} from './ajout/ajout.component';
+import {FooterComponent} from './footer/footer.component';
+import {HeaderComponent} from './header/header.component';
+import {MenuComponent} from './menu/menu.component';
+import {ModifComponent} from './modif/modif.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {LoginComponent} from './login/login.component';
+import {AddUniversiteComponent} from './Universite/Components/AddUniversite/add-universite/add-universite.component';
+import {
+    ListUniversiteComponent
+} from './Universite/Components/ListUniversite/list-universite/list-universite.component';
+import {
+    UpdateUniversiteComponent
+} from './Universite/Components/UpdateUniversite/update-universite/update-universite.component';
+import {E404Component} from './e404/e404.component';
+import {ChambreComponent} from "./chambre/chambre.component";
+import {NgxDatatableModule} from "@swimlane/ngx-datatable";
+import {HttpClientModule} from "@angular/common/http";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TableComponent,
-    AjoutComponent,
-    FooterComponent,
-    HeaderComponent,
-    MenuComponent,
-    ModifComponent,
-    DashboardComponent,
-    LoginComponent,
-    AddUniversiteComponent,
-    ListUniversiteComponent,
-    UpdateUniversiteComponent,
-    E404Component,
-   
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TableComponent,
+        AjoutComponent,
+        FooterComponent,
+        HeaderComponent,
+        MenuComponent,
+        ModifComponent,
+        DashboardComponent,
+        LoginComponent,
+        AddUniversiteComponent,
+        ListUniversiteComponent,
+        UpdateUniversiteComponent,
+        E404Component,
+        ChambreComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NgxDatatableModule,
+        HttpClientModule,
+        NgbModule,
+        ReactiveFormsModule,
+        FormsModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
